4bb382acf4f1509bd35665802dd5a0d7fe95d624
Tan Nguyen <tan@binaty.org>
2016-08-24 00:27:27 +0700

    Update Schema

--- schema/mysql.sql
+++ schema/mysql.sql
@@ -1,48 +1,98 @@
-/**
- * Dump table bot_answers
- */
 CREATE TABLE `bot_answers` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `pattern` text,
-  `answer` text NOT NULL,
-  `sources` varchar(255),
-  `type` varchar(50),
-  `status` VARCHAR(20),
+  `id` int(10) UNSIGNED NOT NULL,
+  `pattern` text COLLATE utf8_unicode_ci,
+  `answer` text COLLATE utf8_unicode_ci NOT NULL,
+  `sources` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `type` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `status` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+-- --------------------------------------------------------
 
-/** Dump table bot_leads **/
+--
+-- Table structure for table `bot_leads`
+--
+
 CREATE TABLE `bot_leads` (
-	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `source` VARCHAR(50) DEFAULT 'facebook',
-  `source_id` VARCHAR(100) NOT NULL,
-	`first_name` varchar(255),
-	`last_name` varchar(255),
-  `profile_pic` VARCHAR(500),
-  `locale` VARCHAR(50),
-  `timezone` VARCHAR(10),
-	`gender` varchar(255),
-	`email` VARCHAR (255),
-	`phone` VARCHAR (255),
-	`country` VARCHAR (255),
-	`location` VARCHAR(255),
-  `wait`  VARCHAR (255),
-  `linked_account` VARCHAR (255),
-  `subscribe` TINYINT,
-  `auto_stop` VARCHAR(10),
+  `id` int(10) UNSIGNED NOT NULL,
+  `source` varchar(50) COLLATE utf8_unicode_ci DEFAULT 'facebook',
+  `user_id` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `first_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `last_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `profile_pic` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `locale` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `timezone` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `gender` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `phone` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `country` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `location` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `wait` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `linked_account` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `subscribe` tinyint(4) DEFAULT NULL,
+  `auto_stop` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-  `deleted_at` timestamp DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-/** Dump table bot_leads_meta **/
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bot_leads_meta`
+--
+
 CREATE TABLE `bot_leads_meta` (
-  `id` int(0) UNSIGNED NOT NULL  AUTO_INCREMENT,
-  `lead_id` INT(10) UNSIGNED NOT NULL,
-  `meta_key` VARCHAR(255),
-  `meta_value` LONGTEXT,
-  PRIMARY KEY (`id`)
+  `id` int(10) UNSIGNED NOT NULL,
+  `lead_id` varchar(30) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `meta_key` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `meta_value` longtext COLLATE utf8_unicode_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bot_answers`
+--
+ALTER TABLE `bot_answers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `bot_leads`
+--
+ALTER TABLE `bot_leads`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `source__user_id` (`source`,`user_id`);
+
+--
+-- Indexes for table `bot_leads_meta`
+--
+ALTER TABLE `bot_leads_meta`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `lead_id__meta_key` (`lead_id`,`meta_key`);
+
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bot_answers`
+--
+ALTER TABLE `bot_answers`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `bot_leads`
+--
+ALTER TABLE `bot_leads`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `bot_leads_meta`
+--
+ALTER TABLE `bot_leads_meta`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
