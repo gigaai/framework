ecb93bf38c949264f7e57903bc6c6cd24183dace
Tan Nguyen <tan@binaty.org>
2016-10-21 01:34:42 +0700

    Comments

--- src/Shared/EasyCall.php
+++ src/Shared/EasyCall.php
@@ -2,21 +2,40 @@
 
 namespace GigaAI\Shared;
 
+/**
+ * EasyCall trait lets you call the class method with both static and non static way
+ *
+ * @package GigaAI\Shared
+ */
 trait EasyCall
 {
+    /**
+     * When you call non static
+     *
+     * @param $name
+     * @param array $args
+     * @return mixed
+     */
     public function __call($name, $args = array())
     {
         return call_user_func_array(array($this, $name), $args);
     }
 
+    /**
+     * When you call static
+     *
+     * @param $name
+     * @param array $args
+     * @return mixed
+     */
     public static function __callStatic($name, $args = array())
     {
+        // Check if class is single ton
         if (method_exists( __CLASS__, 'getInstance')) {
             return self::getInstance()->__call($name, $args);
         }
 
         $self = new self;
-
         return $self->__call($name, $args);
     }
 }
