b31209b60bb77af6873d9ec4b35ea23931f4d45a
Tan Nguyen <tan@binaty.org>
2016-10-25 01:48:21 +0700

    Update schema

--- schema/mysql.sql
+++ schema/mysql.sql
@@ -1,17 +1,16 @@
+
+-- --------------------------------------------------------
+
 --
--- Table structure for table `bot_answers`
+-- Table structure for table `bot_instances`
 --
 
-CREATE TABLE `bot_answers` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `pattern` text COLLATE utf8_unicode_ci,
-  `answers` text COLLATE utf8_unicode_ci NOT NULL,
-  `sources` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `type` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `status` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE `bot_instances` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `meta` text,
+  `status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -21,6 +20,7 @@ CREATE TABLE `bot_answers` (
 
 CREATE TABLE `bot_leads` (
   `id` int(10) UNSIGNED NOT NULL,
+  `instance_id` int(10) UNSIGNED DEFAULT NULL,
   `source` varchar(50) COLLATE utf8_unicode_ci DEFAULT 'facebook',
   `user_id` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   `first_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -37,6 +37,7 @@ CREATE TABLE `bot_leads` (
   `_quick_save` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `linked_account` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `subscribe` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `is_payment_enabled` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
   `auto_stop` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -56,15 +57,62 @@ CREATE TABLE `bot_leads_meta` (
   `meta_value` longtext COLLATE utf8_unicode_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bot_messages`
+--
+
+CREATE TABLE `bot_messages` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `instance_id` int(10) UNSIGNED DEFAULT NULL,
+  `to_lead` text,
+  `to_channel` text,
+  `content` text NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `notification_type` varchar(20) DEFAULT 'REGULAR',
+  `send_limit` varchar(10) DEFAULT '1',
+  `sent_count` int(11) UNSIGNED NOT NULL DEFAULT '0',
+  `routines` varchar(255) DEFAULT NULL,
+  `unique_id` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `start_at` timestamp NULL DEFAULT NULL,
+  `end_at` timestamp NULL DEFAULT NULL,
+  `sent_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bot_nodes`
+--
+
+CREATE TABLE `bot_nodes` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `instance_id` int(10) UNSIGNED DEFAULT NULL,
+  `pattern` text COLLATE utf8_unicode_ci,
+  `answers` text COLLATE utf8_unicode_ci NOT NULL,
+  `wait` varchar(99) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `sources` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `type` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `notification_type` varchar(20) COLLATE utf8_unicode_ci DEFAULT 'REGULAR',
+  `status` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `tags` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `bot_answers`
+-- Indexes for table `bot_instances`
 --
-ALTER TABLE `bot_answers`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `bot_instances`
+  ADD UNIQUE KEY `ix_instance_id` (`id`);
 
 --
 -- Indexes for table `bot_leads`
@@ -81,21 +129,39 @@ ALTER TABLE `bot_leads_meta`
   ADD UNIQUE KEY `lead_id` (`user_id`,`meta_key`);
 
 --
+-- Indexes for table `bot_messages`
+--
+ALTER TABLE `bot_messages`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `unique_id` (`unique_id`);
+
+--
+-- Indexes for table `bot_nodes`
+--
+ALTER TABLE `bot_nodes`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `bot_answers`
---
-ALTER TABLE `bot_answers`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT for table `bot_leads`
 --
 ALTER TABLE `bot_leads`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 --
 -- AUTO_INCREMENT for table `bot_leads_meta`
 --
 ALTER TABLE `bot_leads_meta`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+--
+-- AUTO_INCREMENT for table `bot_messages`
+--
+ALTER TABLE `bot_messages`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `bot_nodes`
+--
+ALTER TABLE `bot_nodes`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
