435839ff3f6c25d86e5c18697ef7ed51e3cb673c
Gary <tan@binaty.org>
2017-04-16 21:53:16 +0700

    Update query scope

--- src/Storage/Eloquent/Lead.php
+++ src/Storage/Eloquent/Lead.php
@@ -17,4 +17,39 @@ class Lead extends \Illuminate\Database\Eloquent\Model
     {
         return $this->first_name . ' ' . $this->last_name;
     }
+    
+    /**
+     * Query scope source
+     *
+     * @param $query
+     * @param $value
+     * @return mixed
+     */
+    public function scopeOfSource($query, $value)
+    {
+        if ( ! empty($value)) {
+            return $query->where('source', $value);
+        }
+        
+        return $query;
+    }
+    
+    /**
+     * Query scope search
+     *
+     * @param $query
+     * @param $value
+     * @return mixed
+     */
+    public function scopeSearch($query, $value)
+    {
+        if ( ! empty($value)) {
+            return $query->where('first_name', 'like', '%' . $value . '%')
+                ->orWhere('last_name', 'like', '%' . $value . '%')
+                ->orWhere('email', 'like', '%' . $value . '%')
+                ->orWhere('phone', 'like', '%' . $value . '%');
+        }
+        
+        return $query;
+    }
 }
