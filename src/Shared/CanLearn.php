69dd18980e4ec5e576f7bc78f2840de4e3214461
Gary Lamb <tan@fitwp.com>
2017-10-08 23:58:43 +0700

    Send message to multiple leads at a time

--- src/Shared/CanLearn.php
+++ src/Shared/CanLearn.php
@@ -7,7 +7,7 @@ trait CanLearn
     /**
      * Response user question with answers
      *
-     * @param $ask
+     * @param      $ask
      * @param null $answers
      *
      * @return $this
@@ -20,7 +20,7 @@ trait CanLearn
     /**
      * Format answer from short hand to proper form.
      *
-     * @param $asks
+     * @param      $asks
      * @param null $answers
      *
      * @return $this For chaining method
@@ -37,13 +37,13 @@ trait CanLearn
      *
      * @param mixed $messages Message to send
      * @param array $attributes Message attributes
-     * @param $lead_id Lead ID to send, by default current lead
+     * @param       $lead_ids Lead ID to send, by default current lead
      *
      * @return $this
      */
-    public function say($messages, $attributes = [], $lead_id = null)
+    public function say($messages, $attributes = [], $lead_ids = null)
     {
-        return $this->says($messages, $attributes, $lead_id);
+        return $this->says($messages, $attributes, $lead_ids);
     }
 
     /**
@@ -51,15 +51,21 @@ trait CanLearn
      *
      * @param mixed $messages Message to send
      * @param array $attributes Message attributes
-     * @param $lead_id Lead ID to send, by default current lead
+     * @param       $lead_ids Lead ID to send, by default current lead
      *
      * @return $this
      */
-    public function says($messages, $attributes = [], $lead_id = null)
+    public function says($messages, $attributes = [], $lead_ids = null)
     {
         $messages = $this->model->parseWithoutSave($messages, $attributes);
 
-        $this->request->sendMessages($messages, $lead_id);
+        if ( ! is_array($lead_ids)) {
+            $this->request->sendMessages($messages, $lead_ids);
+        } else {
+            foreach ($lead_ids as $id) {
+                $this->request->sendMessages($messages, $id);
+            }
+        }
 
         return $this;
     }
@@ -74,18 +80,19 @@ trait CanLearn
         $lead_id = $this->conversation->get('lead_id');
 
         // For chaining after $bot->say() method
-        if ($lead_id != null)
+        if ($lead_id != null) {
             $this->storage->set($lead_id, '_wait', $action);
-
-        // For chaining after $bot->answer() method
-        else
+        } // For chaining after $bot->answer() method
+        else {
             $this->model->addIntendedAction($action);
+        }
     }
 
     /**
      * Index Intended Action
      *
      * @param callable $callback
+     *
      * @return $this
      */
     public function then(callable $callback)
@@ -104,12 +111,13 @@ trait CanLearn
     {
         $previous_intended_action = $this->conversation->get('previous_intended_action');
 
-        if ($previous_intended_action == null)
+        if ($previous_intended_action == null) {
             return;
+        }
 
         $this->says($messages)->wait($previous_intended_action);
     }
-    
+
     public function release()
     {
         $this->run();
@@ -117,19 +125,19 @@ trait CanLearn
 
     public function understand($pattern)
     {
-        
+
     }
 
     public function doesntUnderstand($pattern)
     {
         return ! $this->understand($pattern);
     }
-    
+
     public function isText()
     {
         return ( ! empty($this->message));
     }
-    
+
     public function isPostback()
     {
         return ( ! empty($this->postback));
@@ -139,7 +147,7 @@ trait CanLearn
      * Send typing indicator
      *
      * @param int $delay Typing in seconds
-     * 
+     *
      * @return void
      */
     public function typing($delay = 2)
